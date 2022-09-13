537a54c677f0535561826cf17427137743ba4e80
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2022-09-13 22:50:31 +0200

    Add files via upload

---
+++ zadanie29.js
@@ -0,0 +1,9 @@
+let firstNumber = 89;
+let secondNumber = 196;
+
+function add (firstNumber, secondNumber) {
+  let mmo = firstNumber + secondNumber;
+  return mmo;
+}
+console.log(add(89,196));
+let mmo = add(firstNumber, secondNumber);
