ffa9e70b2d74609cb68c6a45075faf40c0a9a959
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2022-09-13 22:48:37 +0200

    Delete zadanie29.js

--- zadanie29.js
+++
@@ -1,9 +0,0 @@
-let firstNumber ='89';
-let secondNumber ='196';
-
-function add (firstNumber, secondNumber) {
-  let mmo = firstNumber + secondNumber;
-  return mmo;
-}
-console.log(add(89,196));
-let mmo = add(firstNumber, secondNumber);
