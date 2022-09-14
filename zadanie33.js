e0ff606849621bc3ba3701ac0ab5254b46703be2
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2022-09-14 14:37:03 +0200

    Add files via upload

---
+++ zadanie33.js
@@ -0,0 +1,6 @@
+document.getElementById("one-id");
+let button= document.querySelector("button");
+document.querySelector("button").onclick = function() {
+    let desc = document.querySelector('p');
+    desc.textContent='Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto dolorem, quod repudiandae in consectetur recusandae, dolor commodi voluptas unde magnam repellendus aliquid sunt ipsam ex dicta, error odit porro odio?';
+}
