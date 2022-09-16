dff6246e7852c919df33067c6fc86cf1329ac88a
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2022-09-16 22:52:16 +0200

    Add files via upload

---
+++ zadanie36.js
@@ -0,0 +1,12 @@
+let n1 = 20
+let n2 = 10
+
+function compare(num1, num2) {
+    if (num1 + num2 == 20) {
+        return true;
+    } else if (num1 === 20) {
+        return true;
+    } else if (num2 == 20) {
+        return true;
+    }
+}
