f0e392eeab119dd3b50b888dc9022ae89d7ab4d1
oliwiajarguz <112726036+oliwiajarguz@users.noreply.github.com>
2022-09-17 18:19:13 +0200

    Add files via upload

---
+++ zadanie37.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("dodaj").onclick = function add() {
+    document.getElementById("tekst").textContent = "Dodawanie tekstu";
+
+}
+
+document.getElementById("usun").onclick = function del() {
+    document.getElementById("tekst").textContent = "";
+}
